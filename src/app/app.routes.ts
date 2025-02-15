a946ee3b47b6ca6fd16a626be14840854dd3721f
PongpolR <pongpol.r@astartechs.com>
2025-02-15 13:07:13 +0700

    add app model

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoanApplicationComponent } from './loan-application/loan-application.component';
+import { ApplicationListComponent } from './application-list/application-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'newApplication',
+    pathMatch: 'full',
+  },
+  {
+    path: 'newApplication',
+    component: LoanApplicationComponent,
+  },
+  {
+    path: 'applicationList',
+    component: ApplicationListComponent,
+  },
+];
